1b443a0f7b662ab09962d7e87ecdf91dc3f992a3
PedroCrqs <pedrocerqueira0506@gmail.com>
2025-12-09 21:10:40 -0300

    server changed

--- src/scripts/utils/data.js
+++ src/scripts/utils/data.js
@@ -101,7 +101,7 @@ export const listOfGuests = {
   "Juliana family": ["Juliana Branco", "Reynaldo"],
 };
 
-export const RENDER_URL = "https://alice-birthday.onrender.com";
+export const RENDER_URL = "https://alice-birthday.onrender.com/confirm";
 
 /**
  * @param {string} text
